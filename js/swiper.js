351091d3744dc4a4d6831e63d3665e21c91e3f3c
Evan Slaton <evanslaton@gmail.com>
2018-02-11 15:42:56 -0500

    Fixed navbar and swiper issues

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    var swiper = new Swiper(".swiper-container", {
+    var mySwiper = new Swiper(".swiper-container", {
         slidesPerView: 3,
         spaceBetween: 30,
         breakpoints: {
