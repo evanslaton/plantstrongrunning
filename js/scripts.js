6b73bc8b716561e7ee3a872139e5bd81c9091f15
Evan Slaton <evanslaton@gmail.com>
2018-02-13 16:47:50 -0500

    Finished with the 'Why Plant Strong' section media queries on index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,48 @@
 $(document).ready(function() {
-	
-	$(".nav-links").click(function () {
-	    $("#nav-links").slideToggle();
+
+	//Angle Double Down on Home Page
+	$('.nav-links').click(function () {
+	    $('#nav-links').slideToggle();
 	});
+
+
+
+	// let bigImage = $('#img-carousel');
+	// let imgTime;
+	// let i = 0;
+	// let imageArray = [
+	// 'img/food-carousel/1.jpg', 
+	// 'img/food-carousel/2.jpeg', 
+	// 'img/food-carousel/3.jpeg', 
+	// 'img/food-carousel/4.jpeg', 
+	// 'img/food-carousel/5.jpeg', 
+	// 'img/food-carousel/6.png'
+	// ];
+
+	//Image Carousel on Home Page
+	// function changeImageByTimer() {
+	// 	bigImage.animate({opacity: .1, duration: 1000}, function()
+	//   		{$(this).attr('src', imageArray[i])});
+	// 	i++;
+	// 	if (i >= 6) {
+	//     	i = 0;
+	// 	}
+	//   	bigImage.animate({opacity: 1, duration: 1000})
+	// };
+
+	// if ($(window).width() > 750) {
+	// 	imgTimer = setInterval(changeImageByTimer, 5000);
+	// 	bigImage.addClass('on');
+	// }
+
+ //    window.onresize = function() {
+	// 	if ($(window).width() > 750 && !bigImage.hasClass('on')) {
+	// 		imgTimer = setInterval(changeImageByTimer, 5000);
+	// 		bigImage.addClass('on');
+	// 	} else if ($(window).width() < 751 && bigImage.hasClass('on')) {
+	// 		clearInterval(imgTimer);
+	// 		bigImage.removeClass('on');
+	// 	}
+ //    };
 
 });
