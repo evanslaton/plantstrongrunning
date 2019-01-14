236f56e5f806c3dc3401e5240b411a1818795e8e
Evan Slaton <evanslaton@gmail.com>
2019-01-13 21:41:28 -0800

    Contact for is sending emails

--- index.js
+++ index.js
@@ -17,10 +17,28 @@ app.use(bodyParser.urlencoded({extended: true,}));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index')
 });
 
-app.post('/send-email', function(request, response) {
+app.get('/about', function(req, res) {
+  res.render('pages/about')
+});
+
+app.get('/coaching_programs', function(req, res) {
+  res.render('pages/coaching_programs')
+});
+
+app.get('/blog', function(req, res) {
+  res.render('pages/blog')
+});
+
+app.get('/contact', function(req, res) {
+  res.render('pages/contact')
+});
+
+// app.post('/send-email', sendEmail);
+// app.post('/send-contact-info', sendContactInformation);
+app.post('/send-email', function (req, res) {
   let transporter = nodeMailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
@@ -31,20 +49,37 @@ app.post('/send-email', function(request, response) {
     },
   });
 
+  let textBody;
+  if (req.body.emailNewsletter) {
+    textBody = `A vistor to your website has requested to be added to your newsletter mailing list: ${req.body.emailNewsletter}`;
+  } else if (req.body.blogTopic) {
+    textBody = `A visitor to your website would like you to write a blog about ${req.body.blogTopic}`;
+  } else if (req.body.email) {
+    let name = req.body.name ? req.body.name : 'No name given';
+    let tel = req.body.phone ? req.body.phone : 'No phone number given';
+    let message = req.body.message ? req.body.message : 'No message written'
+    textBody = `Awesome! You have a visitor to your website who would like you to contact them!\n
+    Name: ${name}\n
+    Phone: ${tel}\n
+    Email: ${req.body.email}\n
+    Message: ${message}`
+  }
+
   let mailOptions = {
     from: `Web User <${process.env.SENDER}>`, // sender address
     to: process.env.TO, // Recipient
     subject: 'Web Request', // Subject line
-    text: request.body.emailNewsletter, // plain text body
-    html: '<b>Web Request</b>', // html body
+    text: textBody, // plain text body
+    // html: '<b>Web Request</b>', // html body
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       return console.log(error);
     }
+    console.log(req.body.emailNewsletter);
     console.log('Message %s sent: %s', info.messageId, info.response);
-    response.render('index');
+    res.render(req.body.route);
   });
 });
 
